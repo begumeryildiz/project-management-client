38b3ad47bd29ab9bf2f67043fe0e9fb5eb56d4ef
Begum Eryildiz <begumeryildiz@gmail.com>
2022-08-02 16:11:55 +0200

    created context and auth.context.js and added to index.js and navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,28 @@
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 
-function Navbar(){
+function Navbar() {
+
+    const { isLoggedIn, user } = useContext(AuthContext);
+
     return (
         <nav className="Navbar">
             <NavLink to="/">Home</NavLink> |
-            <NavLink to="/projects">Projects</NavLink>
+
+            {isLoggedIn && (
+                <>
+                     <NavLink to="/projects">Projects</NavLink> | 
+                    <button>Logout</button>
+                </>
+            )}
+
+            {!isLoggedIn && (
+                <>
+                    <NavLink to="/signup"> <button>Sign Up</button> </NavLink> |
+                    <NavLink to="/login"> <button>Login</button> </NavLink>
+                </>
+            )}
         </nav>
     );
 }
